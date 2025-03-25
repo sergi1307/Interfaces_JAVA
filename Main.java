3c2f03fc05c65ab7e0c28d26f50c696da75542c6
sergi1307 <sergi13072006@gmail.com>
2025-03-25 12:55:38 +0100

    Hacemos clase principal y método para mostrar los datos

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void pruebaEstadistica() {
+        ArrayListDoubleEstadistica lista = new ArrayListDoubleEstadistica();
+        ArrayDoubleEstadistica array = new ArrayDoubleEstadistica(5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double[] valores = {3.5, 8.9, 26.1, 0.9, 6.7};
+
+        for (double v : valores) {
+            lista.agregar(v);
+            array.agregar(v);
         }
+
+        System.out.println("Estadísticas con ArrayList:");
+        mostrarEstadisticas(lista);
+
+        System.out.println("\nEstadísticas con Array:");
+        mostrarEstadisticas(array);
+
+
+    }
+    public static void mostrarEstadisticas(Estadisticas obj) {
+        System.out.println("Mínimo: " + obj.minimo());
+        System.out.println("Máximo: " + obj.maximo());
+        System.out.println("Media: " + obj.media());
+        System.out.println("Suma: " + obj.suma());
+        System.out.println("Resta: " + obj.resta());
+        System.out.println("Cuantos: " + obj.cuantos());
+    }
+
+    public static void main(String[] args) {
+        pruebaEstadistica();
     }
 }
